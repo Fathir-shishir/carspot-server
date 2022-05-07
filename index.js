20e90b859cff971e67fb66e406938e275b43eb2a
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-05-07 21:15:54 +0200

    all service data send

--- index.js
+++ index.js
@@ -15,12 +15,24 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.te6vn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("dbS connected")
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("carspot").collection("service");
+    app.get('/service',async(req,res)=>{
+    const query ={}
+    const cursor = serviceCollection.find(query)
+    const services = await cursor.toArray();
+    res.send(services)
+    })
+    console.log("connected")
+
+  } finally {
+   
+    
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
